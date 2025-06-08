1d5fa57f4147d0fdf9632357628432847ecd006b
Said Mendoza <114017329+SaidMg10@users.noreply.github.com>
2025-06-07 23:00:44 -0500

    Hello to NaminConv

---
+++ basics/variables.go
@@ -0,0 +1,33 @@
+package basics
+
+import "fmt"
+
+var middleName = "Cane"
+
+func main() {
+	// var age int
+	// var name string = "Jhon"
+	// name1 := "Jane"
+
+	// count := 10
+	// lastName := "Smith"
+	// middleName := "Cane"
+	// middleName := "Mayor"
+
+	fmt.Println(middleName)
+
+	// Default values
+	// Numeric Types: 0
+	// Boolean Types: false
+	// String Types: ""
+	// Pointers, slices, maps, functions, and structs: nil
+	//
+	//  ---- SCOPE
+
+	// fmt.Println(firstName)
+}
+
+func printName() {
+	firstName := "Michale"
+	fmt.Println(firstName)
+}
---
+++ naming_conventions.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type Employee struct {
+	FirstName string
+	LastName  string
+	Age       int
+}
+
+func main() {
+	// PascalCase
+	// Structs, interfaces, enums
+
+	// snake_case
+	// Commonly use for naming variables, constants and filenames
+	// Eg. user_id, first_name, http_request
+
+	// UPPERCASE
+	// Is used exclusively for naming constants in go
+	// Use case is constants
+
+	// Preferly use this for variables than use snake_case
+	// Eg. javaScript, htmlDocument, isValid
+
+	// Package names should be in lowercase
+
+	const MAXRETRIES = 5
+
+	employeeID := 1001
+	fmt.Println("EmployeeID: ", employeeID)
+}
