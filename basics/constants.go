8217bd5c3e2a4c8274d3311dbdc2d5d320d03dc5
Said Mendoza <114017329+SaidMg10@users.noreply.github.com>
2025-06-08 10:04:46 -0500

    arithmetic operations

---
+++ basics/constants.go
@@ -0,0 +1,16 @@
+package basics
+
+const (
+	pi      = 3.14
+	GRAVITY = 9.81
+)
+
+func main() {
+	const days int = 7
+	const (
+		monday       = 1
+		tuesday      = 2
+		wenesday     = 3
+		thursday int = 4
+	)
+}
